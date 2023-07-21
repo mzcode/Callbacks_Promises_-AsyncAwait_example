60e1fc486ca183c554634dd6c417469049c89e4e
wolf <wlf@me.com>
2023-07-21 05:57:23 +0300

    code befor using callback

---
+++ callback.js
@@ -0,0 +1,21 @@
+const posts = [
+  { title: "post one", boady: "this is post one" },
+  { title: "post two", boady: "this is post two" },
+];
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+function creatPost(post) {
+  setTimeout(() => {
+    posts.push(post);
+  }, 2000);
+}
+
+getPosts();
+creatPost({ title: "post three", body: "this post three" });
