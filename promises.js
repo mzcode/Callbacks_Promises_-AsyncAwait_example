ef1e27ad1387dde93d251790be9ca525980c7957
wolf <wlf@me.com>
2023-07-21 07:20:11 +0300

    async/await example

--- promises.js
+++ promises.js
@@ -29,15 +29,23 @@ function creatPost(post) {
 //   .then(getPosts)
 //   .catch((err) => console.log(err));
 
+//Async / Awaite
+
+async function init() {
+  await creatPost({ title: "post three", body: "this post three" });
+
+  getPosts();
+}
+init()
 //promise.all
-const promise1 = Promise.resolve("hello  world");
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, "goodbay")
-);
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
-  (res) => res.json()
-);
-Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-  console.log(values)
-);
+// const promise1 = Promise.resolve("hello  world");
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, "goodbay")
+// );
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+//   (res) => res.json()
+// );
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//   console.log(values)
+// );
